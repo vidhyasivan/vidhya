d2c948336360ffe4a036db2e12f7dea6685651f8
vidhyasivan <vidyasrv98@gmail.com>
2016-06-22 23:17:00 +0530

    Create leap.c

---
+++ leap.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main(void) {
+	int i;
+	scanf("%d",&i);
+	if(i%4==0)
+	printf("entered year is leap year");
+	else
+	printf("entered year is non leap year");	
+	return 0;
+}
